e5aefc07a21909bb5a57149c4c9756e17c3ab437
BahaaZidan <io@bahaazidan.com>
2025-11-07 17:03:32 +0200

    working monorepo setup using pnpm workspaces

---
+++ apps/web/src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import { v } from "@gebna/validation";
+
+  const schema = v.object({
+    lolo: v.string(),
+  });
+  
+  $effect(() => {
+    console.log({validation_result: v.safeParse(schema, {lolo: 12})})
+  })
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
