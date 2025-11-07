d4cdc526f20cf7e431aa5325dc6be162c6ceb06a
BahaaZidan <gebnatorky@gmail.com>
2025-11-07 22:23:19 +0200

    Worker successfully processed email!

---
+++ apps/backend/src/email-inbound.ts
@@ -0,0 +1,11 @@
+import * as PostalMime from "postal-mime";
+
+export const email: NonNullable<
+	ExportedHandler<CloudflareBindings, unknown, unknown>["email"]
+> = async (message, bindings, context) => {
+	const parser = new PostalMime.default();
+	const rawEmail = new Response(message.raw);
+	const email = await parser.parse(await rawEmail.arrayBuffer());
+
+	console.log(email);
+};
--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,5 +1,8 @@
 import { v } from "@gebna/validation";
+import { WorkerEntrypoint } from "cloudflare:workers";
 import { Hono } from "hono";
+
+import { email } from "./email-inbound";
 
 const app = new Hono<{ Bindings: CloudflareBindings }>();
 
@@ -12,6 +15,9 @@ app.get("/", (c) => {
 	return c.json(v.parse(schema, lolo));
 });
 
-export default {
-	fetch: app.fetch,
-} satisfies ExportedHandler<CloudflareBindings>;
+export default class extends WorkerEntrypoint<CloudflareBindings> {
+	fetch = app.fetch;
+	email(message: any, env: CloudflareBindings, ctx: any) {
+		return email(message, env, ctx);
+	}
+}
