99511504aa882d7619869d7e92c935a6e839305a
BahaaZidan <gebnatorky@gmail.com>
2025-11-10 11:56:58 +0200

    fix + better  config

--- apps/backend/eslint.config.ts
+++ apps/backend/eslint.config.ts
@@ -1,9 +1,18 @@
+import { readFileSync } from "fs";
 import js from "@eslint/js";
 import { defineConfig } from "eslint/config";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 
+const gitignore = readFileSync(".gitignore", "utf8")
+	.split("\n")
+	.filter(Boolean)
+	.filter((line) => !line.startsWith("#"));
+
 export default defineConfig([
+	{
+		ignores: gitignore,
+	},
 	{
 		files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
 		plugins: { js },
@@ -11,4 +20,25 @@ export default defineConfig([
 		languageOptions: { globals: { ...globals.browser, ...globals.node } },
 	},
 	tseslint.configs.recommended,
+	{
+		rules: {
+			"no-undef": "off",
+			"no-nested-ternary": "error",
+			eqeqeq: "error",
+			"no-unused-vars": "off",
+			"@typescript-eslint/no-unused-vars": [
+				"error",
+				{
+					args: "all",
+					argsIgnorePattern: "^_",
+					caughtErrors: "all",
+					caughtErrorsIgnorePattern: "^_",
+					destructuredArrayIgnorePattern: "^_",
+					varsIgnorePattern: "^_",
+					ignoreRestSiblings: true,
+				},
+			],
+			// 'no-console': 'error',
+		},
+	},
 ]);
--- apps/backend/src/email-inbound.ts
+++ apps/backend/src/email-inbound.ts
@@ -2,7 +2,7 @@ import * as PostalMime from "postal-mime";
 
 export const email: NonNullable<
 	ExportedHandler<CloudflareBindings, unknown, unknown>["email"]
-> = async (message, bindings, context) => {
+> = async (message, bindings, _context) => {
 	const parser = new PostalMime.default();
 	const rawEmail = new Response(message.raw);
 	const email = await parser.parse(await rawEmail.arrayBuffer());
