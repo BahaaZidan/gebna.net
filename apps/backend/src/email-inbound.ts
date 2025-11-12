d7e24a1eca8766d6591d1d5bc54669c707b138cc
BahaaZidan <io@bahaazidan.com>
2025-11-12 18:15:36 +0200

    test bcc

--- apps/backend/src/email-inbound.ts
+++ apps/backend/src/email-inbound.ts
@@ -1,4 +1,10 @@
+// import { v } from "@gebna/validation";
+// import { inArray } from "drizzle-orm";
 import * as PostalMime from "postal-mime";
+
+// import { getDB } from "./db";
+
+// import { userTable } from "./db/schema";
 
 export const email: NonNullable<
 	ExportedHandler<CloudflareBindings, unknown, unknown>["email"]
@@ -7,6 +13,32 @@ export const email: NonNullable<
 	const rawEmail = new Response(message.raw);
 	const email = await parser.parse(await rawEmail.arrayBuffer());
 
-	console.log(email);
-	console.log(bindings);
+	const rawFrom = email.headers.find((h) => h.key.toLowerCase() === "from")?.value.split(",");
+	if (rawFrom?.length !== 1) return message.setReject("Message have more than one sender.");
+
+	// const deliveredTo = email.deliveredTo?.trim().toLowerCase();
+	// const allRecipients = [...(email.to ?? []), ...(email.cc ?? []), ...(email.bcc ?? [])];
+	// if (!deliveredTo && allRecipients.length === 0)
+	// 	return message.setReject("No valid recipient headers found.");
+
+	const cf_auth_test_header_names = [
+		"dkim-signature",
+		"arc-authentication-results",
+		"arc-message-signature",
+		"arc-seal",
+		"authentication-results",
+		"received-spf",
+		"arc-authentication-results",
+	];
+
+	const { from, to, cc, bcc, deliveredTo, sender } = email;
+	console.log("POSTAL MIME =>", { from, to, cc, bcc, deliveredTo, sender });
+	console.log("MESSAGE =>", { from: message.from, to: message.to });
+	// const authenticationResultss = headers.find((h) => h.key === "Authentication-Results");
+
+	// const db = getDB(bindings);
+	// const doesEmailExist = await db.select().from(userTable).where(inArray(userTable.username, processedHeadersValidation.output.))
+
+	// console.log(email);
+	// console.log(bindings);
 };
