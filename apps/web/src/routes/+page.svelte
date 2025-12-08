aded293deb70c93e4b5faa8538868d44edf902e7
BahaaZidan <gebnatorky@gmail.com>
2025-12-08 13:51:01 +0200

    favicon + daisy

--- apps/web/src/routes/+page.svelte
+++ apps/web/src/routes/+page.svelte
@@ -10,8 +10,4 @@
 	});
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>
-	Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a>
-	to read the documentation
-</p>
+<h1>PRODUCT PAGE</h1>
