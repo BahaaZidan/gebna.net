8114ab6493eeed56733c126d8fef5219891c602d
BahaaZidan <gebnatorky@gmail.com>
2026-01-05 13:43:17 +0200

    init houdini

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,9 +1,10 @@
+import houdini from "houdini/vite";
 import { sveltekit } from "@sveltejs/kit/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()],
+	plugins: [houdini(), tailwindcss(), sveltekit()],
 	server: {
 		port: 5174,
 	},
