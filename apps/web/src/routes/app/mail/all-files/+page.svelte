cab314e4610bb65b2d529ddef00e84f1a2bba715
BahaaZidan <gebnatorky@gmail.com>
2026-01-06 18:03:09 +0200

    use search params to keep track of the filtering state in all-files page

--- apps/web/src/routes/app/mail/all-files/+page.svelte
+++ apps/web/src/routes/app/mail/all-files/+page.svelte
@@ -1,8 +1,7 @@
 <script lang="ts">
 	import AsteriskIcon from "@lucide/svelte/icons/asterisk";
 	import UsersRoundIcon from "@lucide/svelte/icons/users-round";
-
-	import type { AttachmentType$options } from "$houdini";
+	import { useSearchParams } from "runed/kit";
 
 	import Container from "$lib/components/Container.svelte";
 	import AttachmentListItem from "$lib/components/mail/AttachmentListItem.svelte";
@@ -10,25 +9,22 @@
 	import { ATTACHMENT_TYPES } from "$lib/mail";
 
 	import type { PageData } from "./$houdini";
+	import { searchParamsSchema } from "./schema";
 
 	let props: { data: PageData } = $props();
 	let allFilesPageQuery = $derived(props.data.AllFilesPageQuery);
+	let attachments = $derived($allFilesPageQuery.data?.viewer?.attachments.edges.map((e) => e.node));
+	let contacts = $derived($allFilesPageQuery.data?.viewer?.contacts.edges.map((e) => e.node));
 
-	let attachmentType: AttachmentType$options | null = $state(null);
-	let contactAddress: string | null = $state(null);
-
-	const attachments = $derived(
-		$allFilesPageQuery.data?.viewer?.attachments.edges.map((e) => e.node)
-	);
-	const contacts = $derived($allFilesPageQuery.data?.viewer?.contacts.edges.map((e) => e.node));
+	let params = useSearchParams(searchParamsSchema, { pushHistory: false });
 </script>
 
 <Navbar viewer={$allFilesPageQuery.data?.viewer} />
 <Container>
 	<h1 class="divider text-5xl font-semibold">All Files</h1>
 	<h3 class="mt-2 flex items-baseline gap-1">
-		<select class="select" bind:value={attachmentType}>
-			<option value={null}><AsteriskIcon /> All Files</option>
+		<select class="select" bind:value={params.attachmentType}>
+			<option value=""><AsteriskIcon /> All Files</option>
 			{#each ATTACHMENT_TYPES as at (at.type)}
 				<option value={at.type}><at.icon /> {at.name}</option>
 			{/each}
@@ -36,8 +32,8 @@
 
 		<span class="w-40 text-lg">sent by</span>
 
-		<select class="select" bind:value={contactAddress}>
-			<option value={null}><UsersRoundIcon /> everyone</option>
+		<select class="select" bind:value={params.contactAddress}>
+			<option value=""><UsersRoundIcon /> everyone</option>
 			{#each contacts as c (c.id)}
 				<option value={c.address}>{c.name}</option>
 			{/each}
