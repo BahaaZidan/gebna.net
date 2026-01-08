b7673cbaafc70e0e1f38cabad057b29e5f12ac90
BahaaZidan <io@bahaazidan.com>
2026-01-08 11:53:42 +0200

    AVATAR_INFERENCE_DENYLIST

--- apps/backend/src/lib/constant.ts
+++ apps/backend/src/lib/constant.ts
@@ -61,3 +61,164 @@ export const ATTACHMENT_TYPE_BY_MIME = (() => {
 	return map;
 })();
 export const DEFAULT_ATTACHMENT_TYPE: AttachmentType = "Other";
+
+export const AVATAR_INFERENCE_DENYLIST = new Set([
+	// Google (consumer)
+	"gmail.com",
+	"googlemail.com",
+
+	// Microsoft consumer mail
+	"outlook.com",
+	"hotmail.com",
+	"live.com",
+	"msn.com",
+
+	// Apple consumer mail
+	"icloud.com",
+	"me.com",
+	"mac.com",
+
+	// Yahoo
+	"yahoo.com",
+	"ymail.com",
+	"rocketmail.com",
+
+	// Proton
+	"proton.me",
+	"protonmail.com",
+	"pm.me",
+
+	// Tutanota
+	"tutanota.com",
+	"tutanota.de",
+	"tutamail.com",
+	"tuta.io",
+
+	// Zoho Mail (consumer)
+	"zohomail.com",
+
+	// Fastmail
+	"fastmail.com",
+	"fastmail.fm",
+
+	// HEY
+	"hey.com",
+
+	// AOL / Verizon legacy ISP mail
+	"aol.com",
+	"verizon.net",
+
+	// GMX
+	"gmx.com",
+	"gmx.net",
+	"gmx.de",
+	"gmx.at",
+	"gmx.ch",
+
+	// Mail.com umbrella
+	"mail.com",
+	"email.com",
+
+	// Yandex
+	"yandex.com",
+	"yandex.ru",
+	"yandex.by",
+	"yandex.kz",
+	"yandex.ua",
+
+	// Outlook regional
+	"outlook.co.uk",
+	"outlook.fr",
+	"outlook.de",
+	"outlook.it",
+	"outlook.es",
+	"outlook.jp",
+	"outlook.in",
+
+	// Hotmail regional
+	"hotmail.co.uk",
+	"hotmail.fr",
+	"hotmail.de",
+
+	// Yahoo regional
+	"yahoo.co.uk",
+	"yahoo.fr",
+	"yahoo.de",
+	"yahoo.es",
+	"yahoo.it",
+	"yahoo.co.jp",
+	"yahoo.ca",
+	"yahoo.com.au",
+	"yahoo.in",
+
+	// iCloud regional
+	"icloud.co.uk",
+
+	// France ISP mail brands
+	"orange.fr",
+	"wanadoo.fr",
+	"laposte.net",
+
+	// Germany ISP mail brands
+	"web.de",
+
+	// UK ISP mail brands
+	"btinternet.com",
+	"sky.com",
+
+	// US ISP mail brands
+	"comcast.net",
+	"xfinity.com",
+	"att.net",
+	"sbcglobal.net", // AT&T (SBC Global acquired by AT&T)
+	"cox.net",
+	"frontier.com",
+	"charter.net",
+	"spectrum.net",
+
+	// Australia ISP mail brands
+	"bigpond.com",
+	"bigpond.net.au",
+
+	// Mail.ru group
+	"mail.ru",
+	"inbox.ru",
+	"list.ru",
+	"bk.ru",
+	"rambler.ru",
+
+	// Seznam (CZ)
+	"seznam.cz",
+	"email.cz",
+
+	// Korea
+	"naver.com",
+	"daum.net",
+	"hanmail.net",
+
+	// China
+	"qq.com",
+	"sina.com",
+	"sina.cn",
+	"sohu.com",
+	"163.com",
+	"126.com",
+
+	// India
+	"rediffmail.com",
+
+	// Italy
+	"libero.it",
+
+	// Smaller privacy / indie providers
+	"hushmail.com",
+	"runbox.com",
+	"posteo.de",
+	"mailbox.org",
+	"disroot.org",
+	"mailfence.com",
+	"startmail.com",
+
+	// Legacy providers
+	"lycos.com",
+]);
