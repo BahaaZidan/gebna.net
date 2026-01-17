78b7ae97d3022c2da9ffc9c49c3b7e9ddf4cec1f
BahaaZidan <gebnatorky@gmail.com>
2026-01-17 18:39:46 +0200

    $derived everywhere

--- apps/web/src/lib/components/forms/RemoveInputButton.svelte
+++ apps/web/src/lib/components/forms/RemoveInputButton.svelte
@@ -1,7 +1,7 @@
 <script lang="ts" generics="T extends Record<string, unknown>, F extends FormPathArrays<T>">
-	import XIcon from '@lucide/svelte/icons/x';
-	import type { ClassValue } from 'svelte/elements';
-	import { arrayProxy, type FormPathArrays, type SuperForm } from 'sveltekit-superforms';
+	import XIcon from "@lucide/svelte/icons/x";
+	import type { ClassValue } from "svelte/elements";
+	import { arrayProxy, type FormPathArrays, type SuperForm } from "sveltekit-superforms";
 
 	interface Props {
 		superform: SuperForm<T>;
@@ -13,13 +13,13 @@
 
 	let { superform, field, index, class: class_, disabled }: Props = $props();
 
-	const { values } = arrayProxy(superform, field);
+	const { values } = $derived(arrayProxy(superform, field));
 
 	function removeItem() {
 		$values = $values.filter((_, i) => i !== index);
 	}
 </script>
 
-<button class={['btn', class_]} type="button" onclick={removeItem} {disabled}>
+<button class={["btn", class_]} type="button" onclick={removeItem} {disabled}>
 	<XIcon />
 </button>
