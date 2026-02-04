decd888067dcff327cd622f0dbee3b9bdd584629
BahaaZidan <gebnatorky@gmail.com>
2026-02-04 10:43:46 +0200

    init ui-components lib + make sure it works w/ tailwind

--- apps/tipo/src/app/App.tsx
+++ apps/tipo/src/app/App.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable jsx-a11y/accessible-emoji */
 import "../../../../global.css";
 
+import { UiComponents } from "@gebna/ui-components";
 import { Text, View } from "react-native";
 
 export const App = () => {
@@ -8,6 +9,7 @@ export const App = () => {
 		<View className="flex gap-2 bg-green-500 p-20">
 			<Text className="font-mono">mono is cool</Text>
 			<Text className="text-3xl">hoba</Text>
+			<UiComponents />
 		</View>
 	);
 };
