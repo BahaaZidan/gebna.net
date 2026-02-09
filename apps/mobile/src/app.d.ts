0ce79555996e0fd02305498a2b1d0dca18f578c2
BahaaZidan <gebnatorky@gmail.com>
2026-02-09 13:31:00 +0200

    hooks.server.ts + app.d.ts in both desktop and mobile

--- apps/mobile/src/app.d.ts
+++ apps/mobile/src/app.d.ts
@@ -1,9 +1,15 @@
 // See https://kit.svelte.dev/docs/types#app
+
+import type { Session } from "@gebna/auth/server";
+
 // for information about these interfaces
 declare global {
 	namespace App {
 		// interface Error {}
-		// interface Locals {}
+		interface Locals {
+			session: Session["session"];
+			user: Session["user"];
+		}
 		// interface PageData {}
 		// interface Platform {}
 	}
