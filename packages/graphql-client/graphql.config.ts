e2e1e2c9ed25804f47bf9713491f650480205100
BahaaZidan <io@bahaazidan.com>
2026-02-16 21:53:13 +0200

    i'm tired

--- packages/graphql-client/graphql.config.ts
+++ packages/graphql-client/graphql.config.ts
@@ -1,9 +1,10 @@
+import { executableSchema } from "@gebna/graphql-server";
 import type { CodegenConfig } from "@graphql-codegen/cli";
+import { printSchema } from "graphql";
 import type { IGraphQLConfig } from "graphql-config";
 
 const config: IGraphQLConfig = {
-	schema: "http://localhost:5190/api/graphql",
-
+	schema: printSchema(executableSchema),
 	documents: [
 		"../ui/src/components/**/*.{svelte}",
 		"../../apps/desktop/src/**/*.{svelte,ts}",
