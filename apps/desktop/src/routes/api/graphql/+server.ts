71d8cb0a08e4470e0a6dcad31d9872f31b663efd
BahaaZidan <gebnatorky@gmail.com>
2026-02-16 21:12:04 +0200

    integrate graphql-server w/ desktop

---
+++ apps/desktop/src/routes/api/graphql/+server.ts
@@ -0,0 +1,42 @@
+import { getDB } from "@gebna/db";
+import { createYoga } from "@gebna/graphql-server";
+
+import { env } from "$env/dynamic/private";
+
+import type { RequestEvent, RequestHandler } from "./$types";
+
+export const GET: RequestHandler = async (event) => {
+	const db = getDB({ authToken: env.TURSO_AUTH_TOKEN, url: env.TURSO_DATABASE_URL });
+	const handler = createYoga<RequestEvent>({
+		db,
+		viewer: event.locals.user,
+		introspection: env.DEVELOPMENT === "true",
+	}).handleRequest;
+	const response = await handler(event.request, event);
+
+	return response;
+};
+
+export const POST: RequestHandler = async (event) => {
+	const db = getDB({ authToken: env.TURSO_AUTH_TOKEN, url: env.TURSO_DATABASE_URL });
+	const handler = createYoga<RequestEvent>({
+		db,
+		viewer: event.locals.user,
+		introspection: env.DEVELOPMENT === "true",
+	}).handleRequest;
+	const response = await handler(event.request, event);
+
+	return response;
+};
+
+export const OPTIONS: RequestHandler = async (event) => {
+	const db = getDB({ authToken: env.TURSO_AUTH_TOKEN, url: env.TURSO_DATABASE_URL });
+	const handler = createYoga<RequestEvent>({
+		db,
+		viewer: event.locals.user,
+		introspection: env.DEVELOPMENT === "true",
+	}).handleRequest;
+	const response = await handler(event.request, event);
+
+	return response;
+};
