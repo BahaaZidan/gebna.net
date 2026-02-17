1555607ae5fb4e6e250b0df47dc50af3e3f79a3c
BahaaZidan <io@bahaazidan.com>
2026-02-18 00:40:18 +0200

    Viewer.identity

--- packages/graphql-server/src/types.ts
+++ packages/graphql-server/src/types.ts
@@ -1,17 +1,7 @@
 import type { Session } from "@gebna/auth/server";
 import type { DBInstance } from "@gebna/db";
 
-type RemoveUndefined<T> = Exclude<T, undefined>;
-
-// make all props required, and remove `undefined` from each prop’s union
-export type PropsToNullable<T> = {
-	[K in keyof T]-?: RemoveUndefined<T[K]>;
-};
-
-// optional: force TS to show the expanded result in hovers
-export type Simplify<T> = { [K in keyof T]: T[K] } & {};
-
 export interface GraphQLResolverContext {
-	viewer: Simplify<PropsToNullable<Session["user"]>>;
+	viewer: Session["user"];
 	db: DBInstance;
 }
