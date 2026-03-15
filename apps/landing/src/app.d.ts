be2d4e99421b427910d5c92c80bfc8047ff7595e
BahaaZidan <gebnatorky@gmail.com>
2026-03-15 14:59:47 +0200

    landing shall be completely static

--- apps/landing/src/app.d.ts
+++ apps/landing/src/app.d.ts
@@ -1,18 +1,8 @@
 // See https://kit.svelte.dev/docs/types#app
 
-import type { Session } from "@gebna/auth/server";
-
 // for information about these interfaces
 declare global {
-	namespace App {
-		// interface Error {}
-		interface Locals {
-			session?: Session["session"];
-			user?: Session["user"];
-		}
-		// interface PageData {}
-		// interface Platform {}
-	}
+	namespace App {}
 }
 
 export {};
