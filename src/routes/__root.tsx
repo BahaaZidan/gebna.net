fa527b1b3d3ff9c1f8e591cfb590bd0c2fdafaf6
BahaaZidan <io@bahaazidan.com>
2026-03-23 15:29:47 +0200

    @fontsource/inter

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -10,6 +10,14 @@ import { getViewer, type Viewer } from "#/lib/auth/viewer";
 
 import appCss from "../styles.css?url";
 
+import "@fontsource/inter/200.css";
+import "@fontsource/inter/300.css";
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/500.css";
+import "@fontsource/inter/600.css";
+import "@fontsource/inter/700.css";
+import "@fontsource/inter/800.css";
+
 let cachedClientViewer: Viewer | null | undefined;
 
 const fetchGraphQL: FetchFunction = async (request, variables) => {
